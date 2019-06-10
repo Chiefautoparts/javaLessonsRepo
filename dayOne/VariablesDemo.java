d9985ea7b20840da80a8c4d959856127e7d2bbce
John Everyman <johneveryman@Johns-MacBook-Pro.local>
2019-06-10 01:43:49 -0700

    variables examples;

---
+++ dayOne/VariablesDemo.java
@@ -0,0 +1,18 @@
+public class VariablesDemo {
+	
+	int num;
+	float pi;
+	double do;
+	char a;
+
+	pi = 3.14f;
+	do = 20.22d;
+	a = 'v';
+
+	int number = 12;
+	float mypi = 3.14f;
+	double decimal = 22.22d;
+	char letter = 'g';
+
+
+}
