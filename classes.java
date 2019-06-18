026d40b4839ea02eee805703c07583ae6274453e
John Everyman <johneveryman@Johns-MacBook-Pro.local>
2019-06-18 16:06:07 -0700

    assignments added

---
+++ classes.java
@@ -0,0 +1,7 @@
+class classes {
+	public static void main (String args[]){
+		String str = new String();
+		Random rand = new Random();
+		Car chevy = new Car();
+	}
+}
---
+++ variables.java
@@ -0,0 +1,17 @@
+class variables {
+	public static void main(String args[]){
+		// Declaring variables
+		int myAge;
+		String myName;
+		boolean isTired;
+		// Assigning values to variables
+		myAge = 27;
+		myName = "Max";
+		isTired = false;
+
+		// Can also assign a value at the same time the variable is declared.
+		int notMyAge = 99;
+		String notMyName = "Fred";
+		boolean isAwake = true;
+	}
+}
